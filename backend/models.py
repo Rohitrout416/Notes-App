46c41259de7d99a398f4a34abc8ae36c5d3d6dc3
Rohitrout416 <rohitiitrout416@gmail.com>
2026-02-17 09:51:55 +0530

    created models for database

---
+++ backend/models.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
+from database import Base
+
+class Note(Base):
+    __tablename__ = "notes"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    body = Column(String)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    created_at = Column(DateTime)
+    
+    user = relationship("User", back_populates="notes")
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key = True, index = True)
+    name = Column(String, index = True)
+
+    notes = relationship("Note", back_populates="user", cascade="all, delete")
+
