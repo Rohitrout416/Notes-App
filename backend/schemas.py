11f46e346586e4b463def2f7e8dd925bdd99036e
Rohitrout416 <rohitiitrout416@gmail.com>
2026-02-18 17:16:20 +0530

    allowed datetime to be created by default

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,12 +1,11 @@
 from pydantic import BaseModel, Field
-from datetime import datetime
 from typing import List, Optional
 from pydantic import ConfigDict
+from datetime import datetime
 
 class NoteBase(BaseModel):
     title: str
     body: str
-    created_at: datetime
 
 class NoteCreate(NoteBase):
     user_id: Optional[int] = None
@@ -14,7 +13,7 @@ class NoteCreate(NoteBase):
 class Note(NoteBase):
     id: int
     user_id: Optional[int] = None
-
+    created_at: datetime
     model_config = ConfigDict(from_attributes=True)
 
 class UserBase(BaseModel):
