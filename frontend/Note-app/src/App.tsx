c840a61555d51a42de6581305338782f8cedf01a
Rohitrout416 <rohitiitrout416@gmail.com>
2026-02-20 16:06:38 +0530

    used map to avoid repetitive calling of NoteCard component

--- frontend/Note-app/src/App.tsx
+++ frontend/Note-app/src/App.tsx
@@ -2,24 +2,61 @@ import NoteCard from "./components/NoteCard";
 import type { Note } from "./types";
 import './components/NoteCard.css'
 import './App.css'
+import NoteForm from "./components/NoteForm";
+import { useState } from "react";
 
 export default function App(){
-    const note: Note = {
+    const notes: Note[] = [{
         title: "Note 1",
         body: "Body 1",
         created_at: "2022-01-01",
         user_id: 1,
         id: 1
-    }
+    },
+{
+        title: "Note 1",
+        body: "Body 1",
+        created_at: "2022-01-01",
+        user_id: 1,
+        id: 1
+    },
+{
+        title: "Note 1",
+        body: "Body 1",
+        created_at: "2022-01-01",
+        user_id: 1,
+        id: 1
+    },
+{
+        title: "Note 1",
+        body: "Body 1",
+        created_at: "2022-01-01",
+        user_id: 1,
+        id: 1
+    },
+{
+        title: "Note 1",
+        body: "Body 1",
+        created_at: "2022-01-01",
+        user_id: 1,
+        id: 1
+    }]
+
+    const [showForm, setShowForm] = useState(false);
     return(
         <div>
+            <div className="CreateNote">
+                <div onClick={()=>setShowForm(!showForm)}>+</div>
+                {/* {showForm ? <NoteForm /> : <></>} */}
+                {showForm && <NoteForm />}
+
+            </div>
             <div className="NoteGrid">
 
-                <NoteCard {...note} />
-                <NoteCard {...note} />
-                <NoteCard {...note} />
-                <NoteCard {...note} />
-            </div>
+                {notes.map(note =>(
+                    < NoteCard key={note.id} {...note} />
+                ))}
+            </div> 
             
         </div>
     )
